253462231d066ca4039548e7eb94ca22199d88fd
Sergio <202305407@alu.comillas.edu>
2025-03-13 00:48:05 +0100

    cross_pasado

--- src/Lab2_6_CV.py
+++ src/Lab2_6_CV.py
@@ -1,3 +1,4 @@
+import numpy as np
 def cross_validation(model, X, y, nFolds):
     """
     Perform cross-validation on a given machine learning model to evaluate its performance.
@@ -44,26 +45,35 @@ def cross_validation(model, X, y, nFolds):
         # Implement Leave One Out CV
         nFolds = X.shape[0]
 
-    # TODO: Calculate fold_size based on the number of folds
-    fold_size = None
+    # Calculate fold_size based on the number of folds
+    fold_size = X.shape[0] // nFolds
 
-    # TODO: Initialize a list to store the accuracy values of the model for each fold
+    # Initialize a list to store the accuracy values of the model for each fold
     accuracy_scores = []
 
+    permuted_indices = np.random.permutation(X.shape[0])  # Generar un orden aleatorio
+    X, y = X[permuted_indices], y[permuted_indices]
+    start = 0
     for i in range(nFolds):
-        # TODO: Generate indices of samples for the validation set for the fold
-        valid_indices = None
+        if i != nFolds-1:
+            end = start + fold_size
+        else:
+            end = X.shape[0]
+        # Generate indices of samples for the validation set for the fold
+        valid_indices = list(range(start, end))
 
-        # TODO: Generate indices of samples for the training set for the fold
-        train_indices = None
+        # Generate indices of samples for the training set for the fold
+        train_indices = [i for i in range(X.shape[0]) if i not in valid_indices]
+        start = end
 
-        # TODO: Split the dataset into training and validation
-        X_train, X_valid = None, None
-        y_train, y_valid = None, None
+        # Split the dataset into training and validation
+        X_train, X_valid = X[train_indices], X[valid_indices]
+        y_train, y_valid = y[train_indices], y[valid_indices]
 
-        # TODO: Train the model with the training set
+        # Train the model with the training set
+        model.fit(X_train, y_train)
+        # Calculate the accuracy of the model with the validation set and store it in accuracy_scores
+        accuracy_scores.append(model.score(X_valid, y_valid))
 
-        # TODO: Calculate the accuracy of the model with the validation set and store it in accuracy_scores
-
-    # TODO: Return the mean and standard deviation of the accuracy_scores
-    return None, None
+    # Return the mean and standard deviation of the accuracy_scores
+    return np.mean(accuracy_scores), np.std(accuracy_scores)
